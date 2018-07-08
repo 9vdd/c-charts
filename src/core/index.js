79ebb7beba9eda3b50f3a81f41332cdad6dbe593
chendapeng <dapeng_chen@foxmail.com>
2018-07-08 14:59:11 +0800

    remove console

--- src/core/index.js
+++ src/core/index.js
@@ -37,7 +37,6 @@ class Chart {
     }
   }
   initChart(chartConfig) {
-    
     const x = scaleLinear()
       .range([50, chartConfig.width + 50])
       .domain(chartConfig.xdomain);
@@ -45,7 +44,6 @@ class Chart {
     const xAxis = axisBottom()
       .scale(x)
 
-    console.log(this.svgHeight);
     this.svg.append('g')
       .attr('class', 'x axis')
       .attr('transform', `translate(0,${chartConfig.height + 10})`)
@@ -84,50 +82,6 @@ class Chart {
 }
 
 function chart(config) {
-  // const svg = select(dom)
-  //     .append('svg')
-  //     .attr('width', 400)
-  //     .attr('height', 400);
-  // const width = select(dom).width;
-  // const height = select(dom).height;
-  // const x = scaleLinear()
-  //       .range([50, 350])
-  //       .domain([0, 300]);
-
-  // const xAxis = axisBottom()
-  //       .scale(x)
-
-  // svg.append('g')
-  // .attr('class', 'x axis')
-  // .attr('transform', 'translate(0,320)')
-  // .call(xAxis);
-
-  // const y = scaleLinear()
-  //       .range([20, 320])
-  //       .domain([300, 0]);
-
-  // const yAxis = axisLeft()
-  //       .scale(y)
-
-  // svg.append('g')
-  // .attr('class', 'y axis')
-  // .attr('transform', 'translate(50,0)')
-  // .call(yAxis);
-
-  // const data = [[10,10], [50,50], [100, 100], [300, 300]];
-
-  // const lineHandler = line()
-  //     .defined(d => d)
-  //     .x(d => x(d[0]))
-  //     .y(d => y(d[1]))
-
-  // const lineData = lineHandler(data);
-  // console.log(lineHandler);
-  // svg.append('path')
-  // .attr('d', () => lineHandler(data))
-  // .style('stroke', 'red');
-
-  // console.log(svg);
   if (typeof config.query === 'string' && config.query.length > 0) {
     const c = new Chart(config);
     return c;
